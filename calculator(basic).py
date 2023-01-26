1b9ad9fb8ff250951380c69a0bdadaead00026ac
Emre Çıbıkçı <110884340+EmreCibikci@users.noreply.github.com>
2023-01-26 15:50:10 +0300

    Update calculator(basic).py
    added some functions

--- calculator(basic).py
+++ calculator(basic).py
@@ -1,8 +1,18 @@
-# define function needed, add,sub, nul, div
+# define function needed, add,sub, nul, div...
 # print options to the user 
 # get user input ( add, sub , nul, div)
 # call the functions
 # while loop to continue the program until user want to exit
+# define function needed, add,sub, nul, div...
+# print options to the user 
+# get user input ( add, sub , nul, div ...)
+# call the functions
+# while loop to continue the program until user want to exit
+
+
+        
+import math
+import sys
 def add(a,b):
     toplama = a+b
     
@@ -22,6 +32,19 @@ def square(a):
 def kalan(a,b):
     yüzde = a%b
     return str(a) + "%" + str(b) + "=" + str(yüzde)
+def tan():
+   tan1 = float(input("enter number: "))
+   return "tan : " + str(math.tan(tan1))
+def sin():
+    sin1 = float(input("enter number: "))
+    return "sin : " + str(math.sin(sin1))
+def cos():
+    cos1 = float(input("enter number: "))
+    return "cos : " + str(math.cos(cos1))
+def log():
+    log1 = float(input("enter number: "))
+    return "log : " + str(math.log(log1))
+
 while True:
     choice =str(input("What is your choice ? \n1.add + \n2.sub - \n3.nul * \n4.div / \n5.square ** \n6.% \n7.exit\n")).lower()
            
@@ -53,6 +76,19 @@ while True:
         value1= int(input("enter a value : "))
         value2= int(input("enter a value : "))  
         result = kalan(value1,value2)
+        print(result)
+    elif choice == "tan" or choice =="7":
+        result =tan()
+        print(result)
+    elif choice == "sin" or choice =="8":
+        result =sin()
+        print(result)
+    elif choice == "cos" or choice =="9":
+        result =tan()
+        print(result)
+    elif choice == "log" or choice =="10":
+        result =log()
         print(result) 
-    elif choice =="exit" or choice == "Exit" or choice == "E" or choice =="7":
-        exit()
+    elif choice =="exit" or choice == "Exit" or choice == "E" or choice =="11":
+        sys.exit(0)
+        #exit()
